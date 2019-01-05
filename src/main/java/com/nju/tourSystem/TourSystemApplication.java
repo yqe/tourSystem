e5d2389ec1d9cf5400a15803e30227ef161ec8ad
yqe <541123989@qq.com>
2019-01-05 16:40:36 +0800

    添加swagger

--- src/main/java/com/nju/tourSystem/TourSystemApplication.java
+++ src/main/java/com/nju/tourSystem/TourSystemApplication.java
@@ -2,9 +2,17 @@ package com.nju.tourSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TourSystemApplication {
+public class TourSystemApplication extends SpringBootServletInitializer {
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TourSystemApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TourSystemApplication.class, args);
