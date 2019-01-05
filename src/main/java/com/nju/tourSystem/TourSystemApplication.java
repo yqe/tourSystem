a460510da20d1e1b68a85b0fd222f6abb2ced4f6
yqe <541123989@qq.com>
2019-01-05 16:05:26 +0800

    initialize

---
+++ src/main/java/com/nju/tourSystem/TourSystemApplication.java
@@ -0,0 +1,14 @@
+package com.nju.tourSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourSystemApplication.class, args);
+	}
+
+}
+
