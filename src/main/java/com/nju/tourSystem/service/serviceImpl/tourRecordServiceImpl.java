fc6e89161e2209d14f43361bb1e4c00a8f3805fe
yqe <541123989@qq.com>
2019-01-09 15:15:53 +0800

    游记bug修复

--- src/main/java/com/nju/tourSystem/service/serviceImpl/tourRecordServiceImpl.java
+++ src/main/java/com/nju/tourSystem/service/serviceImpl/tourRecordServiceImpl.java
@@ -1,6 +1,18 @@
 package com.nju.tourSystem.service.serviceImpl;
 
+import com.nju.tourSystem.entity.tourRecord;
 import com.nju.tourSystem.service.tourRecordService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class tourRecordServiceImpl implements tourRecordService {
+    @Override
+    public boolean addTourRecord(tourRecord tourRecord) {
+        return false;
+    }
+
+    @Override
+    public boolean updateTourRecord(tourRecord tourRecord) {
+        return false;
+    }
 }
